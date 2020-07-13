509c799924fbc2b45ada77e8b5eb9ff9706ccf10
kimsunhong <>
2020-07-13 21:29:53 +0900

    add VStack

--- test_create_new_repository/ContentView.swift
+++ test_create_new_repository/ContentView.swift
@@ -10,9 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        VStack(alignment: .leading) {
         Text("Hello, World!")
+        Spacer()
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
