fb479f7e0e5487c7b7b9ce0ed911b9e444ebf146
kimsunhong <>
2020-07-13 21:04:25 +0900

    create Xcode project

---
+++ test_create_new_repository/test_create_new_repository/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test_create_new_repository
+//
+//  Created by kimsunhong on 2020/07/13.
+//  Copyright © 2020 kimsunhong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
