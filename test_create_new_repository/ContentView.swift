ef00d911dcb496d1a999de94c554cf6196cca1fe
kimsunhong <>
2020-07-14 20:18:57 +0900

    no message

--- test_create_new_repository/ContentView.swift
+++ test_create_new_repository/ContentView.swift
@@ -10,18 +10,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Hello")
-            Text("Hello, World!")
-            Text("master")
+        HStack(alignment: .center, spacing: 0) {
+            
+            Image("profileSample01")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 31, height: 31)
+                .clipShape(Circle())
+                .padding(.trailing, 7)
+            
+            VStack(alignment: .leading) {
+                
+                Text("닉네임12345")
+                    .font(.headline)
+                    .padding(.bottom, 2)
+                
+                Text("역삼동")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+            }
             Spacer()
-        }
+            Image("icEllipsis")
+        }.frame(height: 69)
     }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        Group{
+            ContentView()
+            
+            ContentView().padding(.horizontal, 18)
+                .previewLayout(.sizeThatFits)
+            
+            ContentView().previewLayout(.fixed(width: 375, height: 69))
+            
+        }
     }
 }
---
+++ test_create_new_repository/Step4.swift
@@ -0,0 +1,21 @@
+//
+//  Step4.swift
+//  test_create_new_repository
+//
+//  Created by kimsunhong on 2020/07/14.
+//  Copyright © 2020 kimsunhong. All rights reserved.
+//
+
+import SwiftUI
+
+struct Step4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Step4_Previews: PreviewProvider {
+    static var previews: some View {
+        Step4()
+    }
+}
